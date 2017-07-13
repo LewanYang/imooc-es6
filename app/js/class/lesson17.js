3089c2f265495ca6d6a331fefc11937d0e5287b1
LewanYang <3278353995@qq.com>
2017-07-13 16:21:19 +0800

    module模块化

---
+++ app/js/class/lesson17.js
@@ -0,0 +1,39 @@
+/**
+ * Created by benz on 2017/7/13.
+ */
+{
+    /**
+     *  Module 模块化
+     */
+
+}
+
+// export let A = 123;
+//
+// export function test(){
+//     console.log('test');
+// }
+//
+// export class Hello{
+//     test(){
+//         console.log('class')
+//     }
+// }
+//
+// export let kfc= 'I can be the best!';
+
+let H = 'haha';
+function dk(){
+    console.log('are you ok?')
+}
+class hf{
+    test(){
+        console.log('class')
+    }
+}
+
+export default {
+    H,
+    dk,
+    hf
+}
